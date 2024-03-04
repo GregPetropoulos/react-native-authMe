1fe0f77857c94f6858362da3e26c63f2fbac4f66
GP <gregpetropoulos@yahoo.com>
2024-03-04 18:05:47 -0500

    Login works in ui and confrimed response token from firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../utils/auth';
+
+
 const LoginScreen = () => {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Logging in...' />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 };
 export default LoginScreen;
--- utils/auth.js
+++ utils/auth.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 import { REACT_APP_API_KEY } from 'react-native-dotenv';
 
-const BASE_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${REACT_APP_API_KEY}`;
+const BASE_URL = `https://identitytoolkit.googleapis.com/v1/accounts`;
 
-export async function createUser(email, password) {
-  const response = await axios.post(`${BASE_URL}`, {
+export async function authenticate(mode, email, password) {
+  const url = `${BASE_URL}:${mode}?key=${REACT_APP_API_KEY}`;
+  const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true
   });
+}
+
+export async function createUser(email, password) {
+  await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }
